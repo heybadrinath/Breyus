1334cdc428560bd9e11f088429310a7e6f2fb720
Sahan <sahanangadi123@gmail.com>
2025-03-30 23:43:56 +0530

    upgrade to premium page

--- frontend/src/seller/upgrade.tsx
+++ frontend/src/seller/upgrade.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import {Layout } from "./components";
+import { Layout } from "./components";
 
-const UpgradeBody = () =>{
-    return(
-        <div></div>
+const UpgradeBody = () => {
+    return (
+        <div className="h-[85vh] flex">
+            <div className="border-2 shadow-lg border-[#cccc] w-fit px-20 py-16 rounded-lg mx-auto my-auto flex h-fit flex-col">
+                <h1 className="w-fit m-auto font-extrabold text-4xl">This is a premium Feature</h1>
+                <p className="w-fit m-auto font-bold text-sm">upgrade now to understand why your users Dropped off</p>
+                <button className="w-fit mx-auto my-4 bg-black text-white text-sm px-20 py-2 rounded-lg">Upgrade to core</button>
+            </div>
+        </div>
+
     );
 };
 
-const Upgrade = () =>{
-    return(
-        <Layout Body={<UpgradeBody/>}/>
+const Upgrade = () => {
+    return (
+        <Layout Body={<UpgradeBody />} />
     );
 };
 
