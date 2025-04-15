9c900f07f28f227e2da8a3090279a4ce5d06af65
Sahan <sahanangadi123@gmail.com>
2025-04-15 17:58:54 +0530

    push

--- frontend/src/seller/dashboard.tsx
+++ frontend/src/seller/dashboard.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Scatter, Line, ResponsiveContainer, ComposedChart } from 'recharts';
 import "../seller/css/components.css";
 
-
 type BarGraphDataType = { week: string; storeVisits: number };
 type ScatterGraphDataType = { x: number, y: number };
 
