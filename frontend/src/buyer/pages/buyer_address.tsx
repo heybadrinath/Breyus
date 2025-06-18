26d75fb17d25f0c125263893693f3600fdf91b04
Sahan <sahanangadi123@gmail.com>
2025-06-18 18:05:31 +0000

    fixed the https part

--- frontend/src/buyer/pages/buyer_address.tsx
+++ frontend/src/buyer/pages/buyer_address.tsx
@@ -57,7 +57,6 @@ const BuyerAddress: React.FC = () => {
 
             // Use the correct endpoint that fetches from user_details table
             const response = await fetch(`https://breyus.com/backend/users/me/details`, {
-                // const response = await fetch(`https://breyus.com/backend/users/me/details`, {
                 headers: {
                     'Authorization': `Bearer ${authService.getToken()}`,
                     'Content-Type': 'application/json'
