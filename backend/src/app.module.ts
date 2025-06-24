e395afc7fcc349f71ee522c3a611621c14373081
Sahan <sahanangadi123@gmail.com>
2025-06-24 14:54:02 +0530

    removed unwanted declarations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { getModelForClass } from '@typegoose/typegoose';
 
 
 
