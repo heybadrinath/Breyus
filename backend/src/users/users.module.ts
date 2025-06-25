b534b3abae1cff511fbf009701d83001d64ec496
Sahan <sahanangadi123@gmail.com>
2025-06-25 18:55:05 +0530

    added necessary modules i.e auth users company

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import {User} from './schemas/users.schema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { getModelForClass } from '@typegoose/typegoose';
-
+import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [UsersController],
   providers: [UsersService],
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: getModelForClass(User).schema }])
-  ]
+  controllers: [UsersController]
 })
 export class UsersModule {}
